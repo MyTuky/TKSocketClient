c63b239e348f462a741a5fc9fc1fa52adb6d7eea
MyTuky <362029446MyTuky@users.noreply.github.com>
2018-01-11 22:28:27 +0800

    Initial commit

---
+++ TKSocketClient/ClientForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TKSocketClient
+{
+    partial class ClientForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(60, 24);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(185, 25);
+            this.txtServer.TabIndex = 0;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(60, 72);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(185, 25);
+            this.txtPort.TabIndex = 0;
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(60, 240);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(386, 65);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(60, 117);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(386, 103);
+            this.txtLog.TabIndex = 0;
+            // 
+            // ClientForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 355);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtServer);
+            this.Name = "ClientForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
